08106827190625e92d18309e2fb0b800c3b05871
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-24 21:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-window.promises = [];
+window.promises = [new Promise((resolve,reject)=>{
+	resolve("p1")
+},Math.random()*5000), new Promise((resolve,reject)=>{
+	resolve("p2")
+},Math.random()*5000), new Promise((resolve,reject)=>{
+	resolve("p3")
+},Math.random()*5000), new Promise((resolve,reject)=>{
+	resolve("p4")
+},Math.random()*5000),new Promise((resolve,reject)=>{
+	resolve("p5")
+},Math.random()*5000),
+				  ];
 
+
+Promise.any(promises).then((result)=>{
+	document.getElementById("output").innerHTML=result;
+})
 // Do not change the code above this
 // add your promises to the array `promises`
